d6a1eb0a069228a62382f598701279ceda7dff9b
jshaffer <joel.shaffer@gmail.com>
2015-03-04 16:38:41 -0800

    Added js/ajax calls for more of the tiles.  Added google charting framework and worked in example (commented out).

--- heartful/static/js/script.js
+++ heartful/static/js/script.js
@@ -1,6 +1,34 @@
 var baseUrl = "http://52.10.162.213/"
 var user = { userName : "", userId : "" }
 
+// Load the Visualization API library and the piechart library.
+google.load('visualization', '1.0', {'packages':['corechart']});
+google.setOnLoadCallback(drawAllHRDataChart);
+
+function drawAllHRDataChart () {
+     // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1], 
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+      ]);
+
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+                     'width':400,
+                     'height':300};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+}
+   // ... draw the chart...
+
 function showTile(tileName) {
   $("#tileContent").css("display", "none");
   $(".jumbotron").css("display", "none");
@@ -36,6 +64,22 @@ function showMyDataTile() {
 
   //allow for manual entry
 
+  //do a get call to load some amount of the data
+  var myDataUrl = baseUrl + "myData/" + user.userId
+
+  $.ajax({
+    type: "GET",
+    url: myDataUrl
+  }).done(function(result) {
+    console.log("got some data")
+  }).fail(function(error){
+    console.log("could not get myData")
+    console.log(error)
+  });   
+
+
+
+
   var toAppend = "<h1>My Health Data</h1>";
   toAppend += '<button type="submit" class="btn btn-success" data-toggle="modal" data-target="#manualDataEntryModal">Enter New Data</button>'
   toAppend += "<br><button onclick='goToHomePage()'>Home</button>";
@@ -45,6 +89,7 @@ function showMyDataTile() {
 function showSleepTile() {
   var toAppend = "<h1>Sleep Tile Stuff</h1>";
   toAppend += "<br><button onclick='goToHomePage()'>Home</button>";
+  
   $("#tileDetail").empty().append(toAppend);
 }
 
@@ -86,6 +131,21 @@ function showIntensityTile() {
 }
 
 function showOtherUsersTile() {
+
+  //do a get call to load data about all the users
+  var allDataUrl = baseUrl + "userData" 
+
+  $.ajax({
+    type: "GET",
+    url: allDataUrl
+  }).done(function(result) {
+    console.log("got some data")
+  }).fail(function(error){
+    console.log("could not get all user data")
+    console.log(error)
+  });  
+
+
   var toAppend = "<h1>Other Users Tile Stuff</h1>";
   toAppend += "<br><button onclick='goToHomePage()'>Home</button>";
   $("#tileDetail").empty().append(toAppend);
@@ -97,6 +157,31 @@ function goToHomePage() {
   $("#tileDetail").empty();
 }
 
+function submitManualData() {
+  var hr = $("#manualEntryHR").val();
+  var spo2 = $("#manualEntrySPO2").val();
+  var date = $("#manualEntryDate").val();
+  var tag = $("#manualEntryTag").val();
+
+  var json = { "userId" : user.userId, "day" : date, "tag" : tag, "HR" : hr, "SPO2" : spo2 };
+  var manualDataUrl = baseUrl + "userData";
+
+  $.ajax({
+    type: "POST",
+    url: manualDataUrl,
+    data: json
+  }).done(function(result) {
+      $("#manualEntryHR").val("");
+      $("#manualEntrySPO2").val("");
+      $("#manualEntryDate").val("");
+      $("#manualEntryTag").val("");
+      $("#manualDataEntryModal").toggle();
+  }).fail(function(result) {
+    console.log("failed to upload data manually")
+  });
+
+  console.log(hr + " " + spo2)
+}
 
 function signinCallback(authResult) {
     if (authResult['status']['signed_in']) {
@@ -110,7 +195,7 @@ function signinCallback(authResult) {
               console.log(user.userName + " " + user.userId);
 
               //hide sign in button and show name instead
-              $("#googleSignInButton").css("display" : "none");
+              $("#googleSignInButton").css("display", "none");
               $("#userName").append("<p>" + user.userName + "</p>");
             });
         });
